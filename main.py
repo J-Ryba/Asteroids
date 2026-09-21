68a92da30a23ef3ac20d8d328187570f0fae47b9
J-Ryba <jirka08@icloud.com>
2026-09-21 22:07:41 +0200

    F: player sprite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,6 +9,8 @@ def main():
 
     dt = 0.0
     clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -17,9 +20,16 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
