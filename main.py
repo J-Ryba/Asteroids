8dd915560423976af587ea5ae01704b0248853ab
J-Ryba <jirka08@icloud.com>
2026-09-24 23:28:28 +0200

    Player-asteroid collison

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
+import sys
+
+from circleshape import CircleShape
 
 from player import Player
 
@@ -39,6 +42,13 @@ def main():
 
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+
+
         for object in drawable:
             object.draw(screen)
         pygame.display.flip()
